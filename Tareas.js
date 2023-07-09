8c3d90df532bb6dab9933511f0a83f007a71452b
CongreMaronas <congregacionmaronas@gmail.com>
2023-07-09 16:07:47 -0300

    Update2

--- Tareas.js
+++ Tareas.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+  $(document).ready(function() {
     $.ajax({
-      url: 'https://docs.google.com/spreadsheets/d/1kmwcFrbbZuHxIbjc8Ow13_ANCS1Vib85fgD_pgy6cUQ/export?format=csv',
+        url: 'https://docs.google.com/spreadsheets/d/1kmwcFrbbZuHxIbjc8Ow13_ANCS1Vib85fgD_pgy6cUQ/export?format=csv',
       dataType: 'text',
       success: function(data) {
         var rows = data.split('\n');
@@ -9,7 +9,8 @@ $(document).ready(function() {
         for (var i = 0; i < rows.length; i++) {
           var cells = rows[i].split(',');
           var rowTag = (i === 0) ? 'th' : 'td'; // Use 'th' for the first row (header row), 'td' for other rows
-          table += '<tr>';
+          var rowClass = (i % 2 === 0 && i !== 0) ? 'par-row' : ''; // Apply 'data-row' class to alternate data rows
+          table += '<tr class="' + rowClass + '">';
   
           for (var j = 0; j < cells.length; j++) {
             table += '<' + rowTag + '>' + cells[j] + '</' + rowTag + '>';
