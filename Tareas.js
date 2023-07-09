bfec4b9edf1460b24ec61f8b18a009396b287c9c
CongreMaronas <congregacionmaronas@gmail.com>
2023-07-09 15:46:44 -0300

    First Page

---
+++ Tareas.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $.ajax({
+      url: 'https://docs.google.com/spreadsheets/d/1kmwcFrbbZuHxIbjc8Ow13_ANCS1Vib85fgD_pgy6cUQ/export?format=csv',
+      dataType: 'text',
+      success: function(data) {
+        var rows = data.split('\n');
+        var table = '<table>';
+  
+        for (var i = 0; i < rows.length; i++) {
+          var cells = rows[i].split(',');
+          var rowTag = (i === 0) ? 'th' : 'td'; // Use 'th' for the first row (header row), 'td' for other rows
+          table += '<tr>';
+  
+          for (var j = 0; j < cells.length; j++) {
+            table += '<' + rowTag + '>' + cells[j] + '</' + rowTag + '>';
+          }
+  
+          table += '</tr>';
+        }
+  
+        table += '</table>';
+        $('#spreadsheet-container').html(table);
+      }
+    });
+  });
