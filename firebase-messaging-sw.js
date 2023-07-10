3f63cddd6ca68f2e967179bdf2cac55864fb99de
CongreMaronas <congregacionmaronas@gmail.com>
2023-07-10 17:53:41 -0300

    Notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,11 @@
+importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyC2Oefqkje2PduiQY28ipq0ju1UxoW7s1o",
+    authDomain: "territorios-341218.firebaseapp.com",
+    projectId: "territorios-341218",
+ 
+});
+
+const messaging = firebase.messaging();
