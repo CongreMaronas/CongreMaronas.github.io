8abfee196b785a536aafdeb3984aa777cefcc160
CongreMaronas <congregacionmaronas@gmail.com>
2023-08-20 16:54:17 -0300

    Nueva tabla de asignados

--- tareas.js
+++ tareas.js
@@ -1,26 +1,77 @@
-  $(document).ready(function() {
-    $.ajax({
-        url: 'https://docs.google.com/spreadsheets/d/1kmwcFrbbZuHxIbjc8Ow13_ANCS1Vib85fgD_pgy6cUQ/export?format=csv',
-      dataType: 'text',
-      success: function(data) {
-        var rows = data.split('\n');
-        var table = '<table>';
-  
-        for (var i = 0; i < rows.length; i++) {
-          var cells = rows[i].split(',');
-          var rowTag = (i === 0) ? 'th' : 'td'; // Use 'th' for the first row (header row), 'td' for other rows
-          var rowClass = (i % 2 === 0 && i !== 0) ? 'par-row' : ''; // Apply 'data-row' class to alternate data rows
-          table += '<tr class="' + rowClass + '">';
-  
-          for (var j = 0; j < cells.length; j++) {
-            table += '<' + rowTag + '>' + cells[j] + '</' + rowTag + '>';
-          }
-  
-          table += '</tr>';
+  // ID de tu hoja de cálculo de Google Docs
+  var spreadsheetId = "ID_DE_TU_HOJA_DE_CÁLCULO";
+
+  // URL de la API de Google Sheets
+  var csvUrl = "https://docs.google.com/spreadsheets/d/1kmwcFrbbZuHxIbjc8Ow13_ANCS1Vib85fgD_pgy6cUQ/export?format=csv";
+
+
+  var columna = 1;
+  var fecha = document.getElementById("date");
+
+  // Función para cargar los datos desde Google Sheets
+  function loadCsvData() {
+      fetch(csvUrl)
+          .then(response => response.text())
+          .then(data => {
+              var table = document.getElementById("csvTable");
+              var lines = data.split('\n');
+
+              // Limpia cualquier dato existente en la tabla
+              table.innerHTML = "";
+
+              // Procesa las líneas del archivo CSV
+              var headers = lines[0].split(',');
+              var fecha = document.getElementById("date");
+              
+              for (var i = 1; i < 11; i++) {
+                  var cells = lines[columna].split(',');
+                  var row = table.insertRow();
+                   
+                  
+                  //Cambiar fecha
+                  fecha.textContent = cells[0];
+
+                  // Agrega columna de encabezado
+                  var headerCell = row.insertCell(0);
+                  headerCell.textContent = headers[i]; // Restar 1 para evitar desbordamiento
+
+                  // Agrega columna de valor
+                  var valueCell = row.insertCell(1);
+                  valueCell.textContent = cells[i]; // Restar 1 para evitar desbordamiento
+
+                  
+              }
+          })
+          .catch(error => {
+              console.error("Error al cargar el archivo CSV:", error);
+          });          
+  }
+
+  // Carga los datos al cargar la página
+  window.addEventListener("load", loadCsvData);
+
+  // Botones
+
+  var buttonPlus = document.getElementById("buttonPlus");
+  var buttonMinus = document.getElementById("buttonMinus");
+
+
+function columnPlus(){
+    if (columna < 9){
+        columna++;
+        loadCsvData();
+        console.log("aaaa");
+    }
+};
+
+    
+
+    function columnMinus(){
+        if(columna > 1){
+            columna--;
+            loadCsvData();
         }
-  
-        table += '</table>';
-        $('#spreadsheet-container').html(table);
-      }
-    });
-  });
+    };
+
+       
+   
